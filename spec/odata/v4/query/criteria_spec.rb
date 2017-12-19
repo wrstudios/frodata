6d89228a3a0edbbbf5414e4a35dedef8876b6c61
Christoph Wagner <christoph@wrstudios.com>
2017-12-18 16:36:09 -0800

    Refactoring Criteria spec

--- spec/odata/v4/query/criteria_spec.rb
+++ spec/odata/v4/query/criteria_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
 
+shared_examples 'a valid criterium' do |operator, value, to_s|
+  it { expect(criteria).to eq(subject) }
+  it { expect(criteria.operator).to eq(operator) }
+  it { expect(criteria.value).to eq(value) }
+  it { expect(criteria.to_s).to eq(to_s) }
+end
+
 describe OData::Query::Criteria do
   let(:string_property) { OData::Properties::String.new(:Name, nil) }
-  let(:integer_property) { OData::Properties::Integer.new(:Name, nil) }
+  let(:integer_property) { OData::Properties::Integer.new(:Age, nil) }
   let(:subject) { OData::Query::Criteria.new(property: string_property) }
 
   it { expect(subject).to respond_to(:property) }
@@ -12,68 +19,51 @@ describe OData::Query::Criteria do
   it { expect(subject).to respond_to(:value) }
   it { expect(subject).to respond_to(:to_s) }
 
-  it { expect(subject).to respond_to(:eq) }
-  it { expect(subject).to respond_to(:ne) }
-  it { expect(subject).to respond_to(:gt) }
-  it { expect(subject).to respond_to(:ge) }
-  it { expect(subject).to respond_to(:lt) }
-  it { expect(subject).to respond_to(:le) }
+  describe 'comparison operators' do
+    describe '#eq' do
+      let(:criteria) { subject.eq('Bread') }
 
-  describe 'operator method' do
-    it '#eq sets up criteria properly' do
-      criteria = subject.eq('Bread')
-      expect(criteria).to eq(subject)
-      expect(criteria.operator).to eq(:eq)
-      expect(criteria.value).to eq('Bread')
-      expect(criteria.to_s).to eq("Name eq 'Bread'")
+      it { expect(subject).to respond_to(:eq) }
+      it_behaves_like 'a valid criterium', :eq, 'Bread', "Name eq 'Bread'"
     end
 
-    it '#ne sets up criteria properly' do
-      criteria = subject.ne('Bread')
-      expect(criteria).to eq(subject)
-      expect(criteria.operator).to eq(:ne)
-      expect(criteria.value).to eq('Bread')
-      expect(criteria.to_s).to eq("Name ne 'Bread'")
+    describe '#ne' do
+      let(:criteria) { subject.ne('Bread') }
+
+      it { expect(subject).to respond_to(:ne) }
+      it_behaves_like 'a valid criterium', :ne, 'Bread', "Name ne 'Bread'"
     end
 
-    it '#gt sets up criteria properly' do
-      subject = OData::Query::Criteria.new(property: integer_property)
+    describe '#gt' do
+      let(:subject) { OData::Query::Criteria.new(property: integer_property) }
+      let(:criteria) { subject.gt(5) }
 
-      criteria = subject.gt(5)
-      expect(criteria).to eq(subject)
-      expect(criteria.operator).to eq(:gt)
-      expect(criteria.value).to eq(5)
-      expect(criteria.to_s).to eq('Name gt 5')
+      it { expect(subject).to respond_to(:gt) }
+      it_behaves_like 'a valid criterium', :gt, 5, "Age gt 5"
     end
 
-    it '#ge sets up criteria properly' do
-      subject = OData::Query::Criteria.new(property: integer_property)
+    describe '#ge' do
+      let(:subject) { OData::Query::Criteria.new(property: integer_property) }
+      let(:criteria) { subject.ge(5) }
 
-      criteria = subject.ge(5)
-      expect(criteria).to eq(subject)
-      expect(criteria.operator).to eq(:ge)
-      expect(criteria.value).to eq(5)
-      expect(criteria.to_s).to eq('Name ge 5')
+      it { expect(subject).to respond_to(:ge) }
+      it_behaves_like 'a valid criterium', :ge, 5, 'Age ge 5'
     end
 
-    it '#lt sets up criteria properly' do
-      subject = OData::Query::Criteria.new(property: integer_property)
+    describe '#lt' do
+      let(:subject) { OData::Query::Criteria.new(property: integer_property) }
+      let(:criteria) { subject.lt(5) }
 
-      criteria = subject.lt(5)
-      expect(criteria).to eq(subject)
-      expect(criteria.operator).to eq(:lt)
-      expect(criteria.value).to eq(5)
-      expect(criteria.to_s).to eq('Name lt 5')
+      it { expect(subject).to respond_to(:lt) }
+      it_behaves_like 'a valid criterium', :lt, 5, 'Age lt 5'
     end
 
-    it '#le sets up criteria properly' do
-      subject = OData::Query::Criteria.new(property: integer_property)
+    describe '#le' do
+      let(:subject) { OData::Query::Criteria.new(property: integer_property) }
+      let(:criteria) { subject.le(5) }
 
-      criteria = subject.le(5)
-      expect(criteria).to eq(subject)
-      expect(criteria.operator).to eq(:le)
-      expect(criteria.value).to eq(5)
-      expect(criteria.to_s).to eq('Name le 5')
+      it { expect(subject).to respond_to(:le) }
+      it_behaves_like 'a valid criterium', :le, 5, 'Age le 5'
     end
   end
 end
