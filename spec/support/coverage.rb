f1daf0171f6d8d4766ce12d959900d65dd8cb7e3
Christoph Wagner <github@pandawhisperer.net>
2018-01-16 12:00:05 -0800

    Fixing Code Climate test reporter, part 2

--- spec/support/coverage.rb
+++ spec/support/coverage.rb
@@ -1,9 +1,2 @@
-if ENV['CC_TEST_REPORTER_ID']
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
-else
-  require 'simplecov'
-  SimpleCov.start do
-    add_filter '/spec/'
-  end
-end
+require 'simplecov'
+SimpleCov.start
